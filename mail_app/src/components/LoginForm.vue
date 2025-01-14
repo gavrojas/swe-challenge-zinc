91f9fb8b0e76553ca1589970cc5d0f9872506119
Gabriela Rojas <grojas9807@gmail.com>
2025-01-13 23:03:24 -0500

    update host back to https login and register

--- mail_app/src/components/LoginForm.vue
+++ mail_app/src/components/LoginForm.vue
@@ -18,7 +18,8 @@ if (route.query.registered) {
 
 async function login() {
   try {
-    const response = await fetch('http://localhost:8080/auth/login', {
+    // const response = await fetch('http://localhost:8080/auth/login', {
+    const response = await fetch('https://zinc-gavrojas.serveftp.com/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(loggedUser.value),
