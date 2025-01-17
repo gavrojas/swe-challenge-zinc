bbff0730838b870f87bbb875bfdd5a45eee7ff23
Gabriela Rojas <grojas9807@gmail.com>
2025-01-17 10:14:42 -0500

    change compositions API emails

--- mail_app/src/stores/emails.ts
+++ mail_app/src/stores/emails.ts
@@ -1,44 +1,44 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 import type { EmailDocument } from '@/types';
 import { useZincService } from '@/services/utils'
 
 const { apiHandleEmails } = useZincService()
 
-export const useEmailStore = defineStore('email', {
-    state: () => ({
-        emails: [] as EmailDocument[],
-        error: null as string | null,
-        totalResults: 0,
-        itemsPerPage: 0, 
-        searchQuery: '', 
-        searchField: 'body', // por defecto
-        folders: [
-            'all_documents',
-            'inbox',
-            'sent',
-            'sent_items',
-            'deleted_items',
-            'discussion_threads',
-            'tasks',
-            'calendar',
-            'contacts',
-        ],
-    }),
-    actions: {
-        async loadEmails() {
-            try {
-                const termOfSearch = this.searchQuery === '' ? '' : this.searchQuery;
-                const maxResults = this.itemsPerPage + 1;
-                const searchType = termOfSearch === '' ? 'match_all' : 'match'
-                const data = { term: termOfSearch, field: this.searchField, max_results: 30 * maxResults, search_type: searchType };
-                const response = await apiHandleEmails('/ByField', {method: 'POST', data },)
+export const useEmailStore = defineStore('email',  () => {
+    const emails = ref<EmailDocument[]>([]);
+    const error = ref<string | null>(null);
+    const totalResults = ref(0);
+    const itemsPerPage = ref(0);
+    const searchQuery = ref('');
+    const searchField = ref('body'); // por defecto
+    const folders = ref([
+        'all_documents',
+        'inbox',
+        'sent',
+        'sent_items',
+        'deleted_items',
+        'discussion_threads',
+        'tasks',
+        'calendar',
+        'contacts',
+    ]);
 
-                this.emails = response.hits.hits.map((hit: any) => hit._source);
-                this.totalResults = response.hits.total.value
-            } catch (err) {
-                this.error = err instanceof Error ? err.message : 'Error loading emails'
-                console.error('Error loading emails:', this.error)
-            }
-        },
-    },
+    const loadEmails = async () => {
+        try {
+            const termOfSearch = searchQuery.value === '' ? '' : searchQuery.value;
+            const maxResults = itemsPerPage.value + 1;
+            const searchType = termOfSearch === '' ? 'match_all' : 'match';
+            const data = { term: termOfSearch, field: searchField.value, max_results: 30 * maxResults, search_type: searchType };
+            const response = await apiHandleEmails('/ByField', { method: 'POST', data });
+
+            emails.value = response.hits.hits.map((hit: any) => hit._source);
+            totalResults.value = response.hits.total.value;
+        } catch (err) {
+            error.value = err instanceof Error ? err.message : 'Error loading emails';
+            console.error('Error loading emails:', error.value);
+        }
+    };
+
+    return { emails, error, totalResults, itemsPerPage, searchQuery, searchField, folders, loadEmails };
 });
