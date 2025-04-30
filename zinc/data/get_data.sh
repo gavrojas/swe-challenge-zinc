474032021377af637722a20ab3215685d3a201e8
Gabriela Rojas <grojas9807@gmail.com>
2025-04-30 14:11:48 -0500

    modify data file for Render

--- zinc/data/get_data.sh
+++ zinc/data/get_data.sh
@@ -9,6 +9,6 @@ TAR_FILE="enron_mail_20110402.tgz"
 
 # decompress de file in the directory
 echo "Decompressing the file $TAR_FILE ..."
-tar -zxf "$TAR_FILE" -C /var/lib/zinc/
+tar -zxf "$TAR_FILE" -C .
 
 echo "Decompression complete. Files extracted in the directory."
